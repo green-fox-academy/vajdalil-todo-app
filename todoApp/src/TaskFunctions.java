4a28e9b24dd7370fb0ac17d303e37441b3554945
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-14 16:58:02 +0100

    feat(remove function): function is working with error messages

--- todoApp/src/TaskFunctions.java
+++ todoApp/src/TaskFunctions.java
@@ -39,20 +39,28 @@ public class TaskFunctions {
         }
     }
     public void removeTask(String [] args){
-        int number = Integer.parseInt( args[1]);
+        int number;
         try {
-            List<String> lines = readAllLines(filePath);
-            if (lines.size() < 2) {
-                System.out.println("Unable to remove: no index provided");
-            } else if (lines.size() < number) {
-                System.out.println("Unable to remove: index is out of bound");
-            } else {
-                lines.remove(number - 1);
+            number = Integer.parseInt(args[1]);
+            try {
+                List<String> lines = readAllLines(filePath);
+                if (number <= 0 ) {
+                    System.out.println("Unable to remove: no index provided");
+                } else if (lines.size() < number) {
+                    System.out.println("Unable to remove: index is out of bound");
+                } else {
+                    lines.remove(number - 1);
+                }
+                Files.write(filePath,lines);
             }
-            Files.write(filePath,lines);
+            catch (IOException e ) {
+                System.out.println("Unable to read file: " + filePath);
+            }
         }
-        catch (IOException e ) {
-            System.out.println("Unable to read file: " + filePath);
+        catch (Exception e) {
+            System.out.println("Unable to remove: index is not a number");
         }
+
+
     }
 }
