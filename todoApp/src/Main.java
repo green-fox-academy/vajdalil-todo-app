b65c3513ef3012373bdd6dedb2990475a7ae0a81
Vajda Lilla <vajdalil.hu@gmail.com>
2017-12-14 13:38:51 +0100

    feat(todoApp) reorganized the class structure

--- todoApp/src/Main.java
+++ todoApp/src/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
 public static void main(String[] args) {
 
-    ListTasks todoThis = new ListTasks();
+    ListTaskscd ..
+    todoThis = new ListTasks();
 
     if (args.length == 0) {
         System.out.println("Command Line Todo application\n" +
